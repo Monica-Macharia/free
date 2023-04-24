fe05cdcb31b94f6f85089da203e76c0fd993504e
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2023-04-24 12:19:50 +0300

    js

---
+++ index.js
@@ -0,0 +1,15 @@
+// function maize(buy){
+//     return console.log(buy)
+// }
+
+// maize("prices")
+
+let maize = (buy, sell)=>{
+    console.log(buy)
+    console.log(sell)
+}
+maize("foot", "gut")
+
+let mango=[7,3,4,5,6,7,8,9]
+let mangos= mango.push(999)
+console.log(mangos)
